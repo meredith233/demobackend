1459f0092a329de2b8c05229a9c245b7cfaf5085
梁炬鸿 <mrexgo@gmail.com>
2021-02-25 17:12:33 +0800

    init

---
+++ src/test/java/top/mrexgo/demobackend/DemobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mrexgo.demobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
